6167dbd74c22be047a8839faf0a9ad5f470ff365
Phimanesone Thomas <tphiman@gmail.com>
2020-09-17 12:18:56 +0100

    initial commit

---
+++ app/src/main/java/com/example/pokedex/Model/Evolution.kt
@@ -0,0 +1,7 @@
+package com.example.pokedex.Model
+
+class Evolution {
+    var num:String?=null
+    var name:String?=null
+
+}
---
+++ app/src/main/java/com/example/pokedex/Model/Pokedex.kt
@@ -0,0 +1,8 @@
+package com.example.pokedex.Model
+
+class Pokedex {
+    /*
+    public List<Pokemon> pokemon { get; set; }
+     */
+    var pokemon:List<Pokemon>? = null
+}
